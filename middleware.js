9e25a8bd2a0649220aa57fe0466ff88f9b26feed
aeonzz <124867727+aeonzz@users.noreply.github.com>
2024-01-16 17:09:59 +0800

    middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = { 
-    matcher: ["/", "/admin", "/admin/users", "/user", "/user/account", "/events", "/announcements", ] 
+    matcher: ["/", "/admin", "/admin/users", "/user", "/user/account", "/events", "/announcements", "/post/:path*", "/user/:path*" ] 
 }
